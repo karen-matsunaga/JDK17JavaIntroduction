f0f257aaeb05d0e688b078699ef345b1a4f7580c
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-02-06 21:37:13 -0300

    2025-02-06 - Lecture 14 Ternary Operator

--- src/net/karen/Main.java
+++ src/net/karen/Main.java
@@ -2,23 +2,25 @@ package net.karen;
 
 public class Main {
     public static void main(String[] args) {
-        /* Casting (or converting data types between each other) */
+        /* Ternary Operator */
 
-        int loan = 750;
-        float interestRate = 0.0525f;
+        int exam = 65;
+        int gift = 0;
 
-        /* implicit cast -> 750 transform automatically to float datatype
-           explicit cast -> (float) 750 the integer number is forced to transform into float */
-        float interest = loan * interestRate; // implicit cast
-        System.out.println(interest);
+        /* Default code */
+        if (exam >= 50) {
+            gift = 10;
+        } else {
+            gift = 0;
+        }
+        System.out.println(gift);
 
-        // Example using float cast
-        float interest1 = (float) loan * interestRate; // explicit cast
-        System.out.println(interest1);
-
-        // Example using int cast
-        /* float = 0.0525f -> int = 0 */
-        int interest2 = loan * (int) interestRate; // explicit cast interestRate = int 0
-        System.out.println(interest2);
+        /* Ternary Operator - Short code
+           exam >= 50 -> condition
+           10 -> true
+           0 -> false
+        */
+        gift = exam >= 50 ? 10 : 0;
+        System.out.println(gift);
     }
 }
