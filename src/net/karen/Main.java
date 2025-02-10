bab86cfa05783dccb21ebd6f53fb0acc03447d16
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-02-10 15:32:39 -0300

    2025-02-10 - Homework: Trivia Game with Methods #2

--- src/net/karen/Main.java
+++ src/net/karen/Main.java
@@ -1,59 +1,57 @@
 package net.karen;
+import java.util.Scanner;
 
 public class Main {
-
-    // Method main
     public static void main(String[] args) {
-        /* Methods */
 
-        String[] que = new String[3];
+        String[] que = new String[5];
+        que[0] = "Neymar is known in which sport?";
+        que[1] = "Lewis Hamilton was champion how many times in Formula 1?";
+        que[2] = "What is the capital of Japan?";
+        que[3] = "Who created the Java programming language?";
+        que[4] = "What does the acronym WWW mean?";
 
-        que[0] = "How many States does the USA have?";
-        que[1] = "What is the Capital of the United Kingdom?";
-        que[2] = "What is the chemical symbol for Iron?";
+        String[] ans = new String[5];
+        ans[0] = "Football";
+        ans[1] = "7";
+        ans[2] = "Tokyo";
+        ans[3] = "SunMicrosystems";
+        ans[4] = "WorldWideWeb";
 
-        String[] ans = new String[3];
-
-        ans[0] = "50";
-        ans[1] = "London";
-        ans[2] = "Fe";
-
-        // Access custom methods on main method
-
-        // 1. outputQuestionsAndAnswers method
-        outputQuestionsAndAnswers(que, ans);
-
-        // 2. sum method
-        System.out.println(sum(10, 12));
-    }
-
-    // 1. Method outputQuestionsAndAnswers -> Method type void
-    /*
-        public static -> access modifier
-        void -> type of method
-        outputQuestionsAndAnswers -> name method
-        String[] questions, String[] answers -> parameters
-
-        Each method signatures is unique
-    */
-    public static void outputQuestionsAndAnswers(String[] questions, String[] answers) {
-       for (int i = 0; i < questions.length; i++) {
-           System.out.println(questions[i]);
-           System.out.println(answers[i]);
+       // Game Trivia menu
+       while (true) {
+           Scanner scanner = new Scanner(System.in);
+           System.out.print("Play Trivia Game #2? ");
+           int score = 0;
+           if (scanner.next().equals("yes")) {
+               for (int i = 0; i < que.length; i++) {
+                   showQuestions(que, i);
+                   String answer = scanner.next();
+                   if (answer.equals(ans[i])) {
+                       score++;
+                       checkAnswer(ans, i, answer, score);
+                   } else {
+                       checkAnswer(ans, i, answer, score);
+                   }
+               }
+           } else {
+               break;
+           }
        }
     }
 
-    // Error -> Method signatures is UNIQUE
-//    public static void outputQuestionsAndAnswers(String[] questions, String[] answers) {
-//    }
+    // First method - Questions
+    public static void showQuestions(String[] question, int i) {
+        System.out.println(i+1 + "- " + question[i]);
+    }
 
-    // No error -> Remove String[] answers parameter -> different method signature even if it uses name method equal
-    // different 1. number of parameters, 2. types of parameters or 3. return type
-//    public static void outputQuestionsAndAnswers(String[] questions) {
-//    }
-
-    // 2. Method sum -> Method type int
-    public static int sum(int x, int y) {
-        return x + y;
+    // Second method - Answers
+    public static void checkAnswer(String[] answer, int i, String typeAnswer, int score) {
+        if (typeAnswer.equals(answer[i])) {
+            System.out.println("Congratulations the answer is correct! Your Score is: [" + score + "/" + 5 + "]");
+        }
+        else {
+            System.out.println("The answer is wrong! " + answer[i] + ". Your Score is : [" + score + "/" + 5 + "]");
+        }
     }
 }
