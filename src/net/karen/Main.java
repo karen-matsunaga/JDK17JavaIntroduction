493ad1cd157521d67fa013cf6d5e9fd635186cb7
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-02-11 19:49:07 -0300

    2025-02-11 - Lecture 18 Java Collections

--- src/net/karen/Main.java
+++ src/net/karen/Main.java
@@ -1,57 +1,100 @@
 package net.karen;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+    /* ArrayList, Maps and Sets (Collections) */
 
-        String[] que = new String[5];
-        que[0] = "Neymar is known in which sport?";
-        que[1] = "Lewis Hamilton was champion how many times in Formula 1?";
-        que[2] = "What is the capital of Japan?";
-        que[3] = "Who created the Java programming language?";
-        que[4] = "What does the acronym WWW mean?";
+       /* Array */
+       String[] questions = new String[2];
+       questions[0] = "How many States does the USA have?";
+       questions[1] = "Capital of UK?";
 
-        String[] ans = new String[5];
-        ans[0] = "Football";
-        ans[1] = "7";
-        ans[2] = "Tokyo";
-        ans[3] = "SunMicrosystems";
-        ans[4] = "WorldWideWeb";
-
-       // Game Trivia menu
-       while (true) {
-           Scanner scanner = new Scanner(System.in);
-           System.out.print("Play Trivia Game #2? ");
-           int score = 0;
-           if (scanner.next().equals("yes")) {
-               for (int i = 0; i < que.length; i++) {
-                   showQuestions(que, i);
-                   String answer = scanner.next();
-                   if (answer.equals(ans[i])) {
-                       score++;
-                       checkAnswer(ans, i, answer, score);
-                   } else {
-                       checkAnswer(ans, i, answer, score);
-                   }
-               }
-           } else {
-               break;
-           }
+       for (String que : questions) {
+           System.out.println(que);
        }
+
+       /* ArrayList */
+       List<String> questionList = new ArrayList<>();
+       questionList.add("What Language is spoken in Germany?"); // List add index
+       questionList.add("What is the Capital of Canada?");
+       questionList.add("In what Hemisphere is Brazil?");
+
+       /* LISTS -> Add entries / elements dynamically */
+       questionList.remove(1); // List remove index
+       System.out.println(questionList.size()); // List size
+       System.out.println(questionList.get(0)); // List specific index
+
+       printArrayListString(questionList);
+
+       // Wrapper Classes we can use instead of the primitive data types
+       List<Integer> numbers = new ArrayList<>();
+       numbers.add(420); // List add index
+       numbers.add(42);
+
+       printArrayListInteger(numbers);
+
+       // Maps -> Map contains a Key and a Value. Key maps to a certain Value
+       // Key-Value Pair
+       // Map<data type, data type> -> Ex: Map<Int, Float> => Work!
+       Map<String, String> countryToCapital = new HashMap<>();
+       countryToCapital.put("Germany", "Berlin"); // Map add index
+       countryToCapital.put("France", "Paris");
+       countryToCapital.put("Italy", "Rome");
+       countryToCapital.put("US", "Washington DC");
+
+       countryToCapital.get("Germany"); // Map specific index
+
+
+       printMap(countryToCapital);
+
+       System.out.println("Contains Key 'Germany'? " + countryToCapital.containsKey("Germany")); // Map contains Key
+       System.out.println("Contains Value 'London'? " + countryToCapital.containsValue("London")); // Map contains Value
+
+       System.out.println(countryToCapital.remove("France")); // Map remove key
+
+       printMap(countryToCapital);
+
+       // SETS -> A Set is a collection that contains no duplicates
+       Set<String> usernames = new HashSet<>();
+       usernames.add("Kaupenjoe"); // Set add index
+       usernames.add("Nanoattack");
+
+       printSet(usernames);
+
+       System.out.println(usernames.add("FRV")); // True -> Add on Set index
+       System.out.println(usernames.add("FRV")); // False -> no duplicate
+
+       printSet(usernames);
+
     }
 
-    // First method - Questions
-    public static void showQuestions(String[] question, int i) {
-        System.out.println(i+1 + "- " + question[i]);
+    // ERRORS YOU MIGHT RUN INTO
+    // Uncomment for errors
+
+    // IndexOutOfBoundException
+    // If you try and pass in an Index that does not exist for a list
+    // System.out.println(questionList.get(2));
+
+    // Not an Exception, BUT "null"
+    // If you pass in a Key into a Map that does not exist
+    // Your return value is going to be null (might lead to other errors down the line!)
+    // System.out.println(countryToCapital.get("Malta"));
+
+    // My annotations
+    public static void printArrayListString(List<String> arrayList) {
+         System.out.println(arrayList); // List String custom method
     }
 
-    // Second method - Answers
-    public static void checkAnswer(String[] answer, int i, String typeAnswer, int score) {
-        if (typeAnswer.equals(answer[i])) {
-            System.out.println("Congratulations the answer is correct! Your Score is: [" + score + "/" + 5 + "]");
-        }
-        else {
-            System.out.println("The answer is wrong! " + answer[i] + ". Your Score is : [" + score + "/" + 5 + "]");
-        }
+    public static void printArrayListInteger(List<Integer> arrayList) {
+         System.out.println(arrayList); // List Integer custom method
+    }
+
+    public static void printMap(Map<String, String> map) {
+         System.out.println(map); // Map custom method
+    }
+
+    public static void printSet(Set<String> set) {
+         System.out.println(set); // Set custom method
     }
 }
